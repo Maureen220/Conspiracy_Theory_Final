d4ccc0e7db0c69c5684c97f6033657516314e743
[Maureen Bayne] <[maureenbayne34@gmail.com]>
2021-10-27 16:09:39 -0600

    Adding SQL file with potential joins in data

---
+++ Resources/queries.sql
@@ -0,0 +1,143 @@
+--Below are a few of the potential joins one can make in SQL once the data has been loaded from the file <database_connection_to_machine_model.ipynb>
+
+--Here is the code for combining all the tables into one table
+SELECT ts.totalscore,
+	et.et,
+	gs.global_conspiracies, 
+	gm.government_malfeasance, 
+	pw.personal_wellbeing, 
+	co.control_of_info, 
+	co.education, 
+	co.urban, 
+	co.gender, 
+	co.engnat, 
+	co.age, 
+	co.hand, 
+	co.religion, 
+	co.orientation, 
+	co.race,
+	co.voted, 
+	co.married, 
+	co.familysize
+INTO all_combined_table
+FROM control_of_info as co
+LEFT JOIN total_score as ts
+	ON co.index = ts.index
+LEFT JOIN et as et
+	ON et.index = ts.index
+LEFT JOIN global_conspiracies as gs
+	ON gs.index = ts.index
+LEFT JOIN government_malfeasance as gm
+	ON gm.index = ts.index
+LEFT JOIN personal_wellbeing as pw
+	ON pw.index = ts.index;
+
+--combining the control_of_info and total_score tables
+SELECT ts.totalscore,
+	co.control_of_info, 
+	co.education, 
+	co.urban, 
+	co.gender, 
+	co.engnat, 
+	co.age, 
+	co.hand, 
+	co.religion, 
+	co.orientation, 
+	co.race,
+	co.voted, 
+	co.married, 
+	co.familysize
+INTO control_totalscore_table
+FROM control_of_info as co
+LEFT JOIN total_score as ts
+ON co.index = ts.index;
+
+DROP TABLE control_totalscore_table;
+
+--combining the et (extreaterrestrial) and total_score tables
+SELECT ts.totalscore,
+	et.et, 
+	et.education, 
+	et.urban, 
+	et.gender, 
+	et.engnat, 
+	et.age, 
+	et.hand, 
+	et.religion, 
+	et.orientation, 
+	et.race,
+	et.voted, 
+	et.married, 
+	et.familysize
+INTO et_totalscore_table
+FROM et as et
+LEFT JOIN total_score as ts
+ON et.index = ts.index;
+
+DROP TABLE et_totalscore_table;
+
+--combining the global_conspiracies and total_score tables
+SELECT ts.totalscore,
+	gs.global_conspiracies, 
+	gs.education, 
+	gs.urban, 
+	gs.gender, 
+	gs.engnat, 
+	gs.age, 
+	gs.hand, 
+	gs.religion, 
+	gs.orientation, 
+	gs.race,
+	gs.voted, 
+	gs.married, 
+	gs.familysize
+INTO gs_totalscore_table
+FROM global_conspiracies as gs
+LEFT JOIN total_score as ts
+ON gs.index = ts.index;
+
+DROP TABLE gs_totalscore_table;
+
+--combining the government_malfeasance and total_score tables
+SELECT ts.totalscore,
+	gm.global_conspiracies, 
+	gm.education, 
+	gm.urban, 
+	gm.gender, 
+	gm.engnat, 
+	gm.age, 
+	gm.hand, 
+	gm.religion, 
+	gm.orientation, 
+	gm.race,
+	gm.voted, 
+	gm.married, 
+	gm.familysize
+INTO gm_totalscore_table
+FROM government_malfeasance as gm
+LEFT JOIN total_score as ts
+ON gm.index = ts.index;
+
+DROP TABLE gm_totalscore_table;
+
+--combining the personal_wellbeing and total_score tables
+SELECT ts.totalscore,
+	pw.global_conspiracies, 
+	pw.education, 
+	pw.urban, 
+	pw.gender, 
+	pw.engnat, 
+	pw.age, 
+	pw.hand, 
+	pw.religion, 
+	pw.orientation, 
+	pw.race,
+	pw.voted, 
+	pw.married, 
+	pw.familysize
+INTO pw_totalscore_table
+FROM personal_wellbeing as pw
+LEFT JOIN total_score as ts
+ON pw.index = ts.index;
+
+DROP TABLE pw_totalscore_table;
