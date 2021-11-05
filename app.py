ebe3f8b9b48d978a88e139df624e1ab5684d4ab8
[Maureen Bayne] <[maureenbayne34@gmail.com]>
2021-11-04 21:08:19 -0600

    editing our html and css. importing some images. creating file structure

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", prediction= "static/images/mind_control.gif")
+    return render_template("index.html", prediction= "static/images/aliens.gif")
 
 
 @app.route("/predict", methods=["post"])
