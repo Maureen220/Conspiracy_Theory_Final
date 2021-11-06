8706f4e9fcd4d416815ed3b4faf847bc2531334a
[Maureen Bayne] <[maureenbayne34@gmail.com]>
2021-11-05 19:30:42 -0600

    used onclick to create button link

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,21 @@
 d3.csv("../Resources/cleaned_data_removed_outliers.csv").then(function(data){
     console.log(data);
   });
+
+// // d3.csv("../Resources/cleaned_data_removed_outliers.csv").then(makeChart);
+
+// function makeChart(genders){
+//     var male = genders.map(function(d) {return d.gender == 1});
+//     var female = genders.map(function(d) {return d.gender == 2});
+//     var other = genders.map(function(d) {return d.gender == 3});
+
+//     var chart = ('chart', {
+//         type: 'bar', 
+//         data: [male, female, other]
+
+//     })
+// }
+
+// // Request data using D3
+// d3.csv("../Resources/cleaned_data_removed_outliers.csv")
+//   .then(makeChart);
