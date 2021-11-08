46267c8f7794129a37c4ca6133cfaba9b3581ce2
[Maureen Bayne] <[maureenbayne34@gmail.com]>
2021-11-08 15:35:44 -0700

    Cleaned up predictions.html and added css styles. updated app.py

--- app.py
+++ app.py
@@ -14,12 +14,37 @@ def predict():
     output = "Your value here!"
     #If you have the user submit a form
     if request.method == 'POST': 
-        age = request.form.get("age")
-        religion = request.form.get("religion")
-        print(age)
-        print(religion)
-        output = model(age, religion)
-        return render_template('predictions.html', output=output)
+        """
+        age: age,
+            religion: religion,
+            family_size: family_size,
+            urban: urban,
+            gender: gender,
+            education: education,
+            engant: engant,
+            hand_orientation: hand_orientation,
+            orientation: orientation, 
+            race: race,
+            voted: voted,
+            married: married
+"""
+        age=request.json.get("age")
+        religion=request.json.get("religion")
+        family_size=request.json.get("family_size")
+        urban=request.json.get("urban")
+        gender=request.json.get("gender")
+        education=request.json.get("education")
+        engant=request.json.get("engant")
+        hand_orientation=request.json.get("hand_orientation")
+        orientation=request.json.get("orientation")
+        race=request.json.get("race")
+        voted=request.json.get("voted")
+        married=request.json.get("married")
+        pred=model.predict([[education, urban, gender, engant, age, hand_orientation, religion, orientation, race, voted, married, family_size]])
+
+        print(pred)
+        # return render_template('predictions.html', output=output)
+        return {"Prediction": pred[0]}
         
     return render_template('predictions.html', output=output)
 
